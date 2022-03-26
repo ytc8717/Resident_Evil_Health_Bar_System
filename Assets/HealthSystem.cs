ea6d0f4423d7ff8d9eafc71f98104cc1aa4ba74b
YTC <60242731+ytc8717@users.noreply.github.com>
2022-03-26 03:08:16 -0700

    Update

--- Assets/HealthSystem.cs
+++ Assets/HealthSystem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScrollHealth : MonoBehaviour
+public class HealthSystem : MonoBehaviour
 {
     Sprite healthStatus;
     Sprite healthRate;
